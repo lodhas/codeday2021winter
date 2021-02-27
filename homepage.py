9ff6761a9dc2a3a5dfd3a4953687ea68878971f3
arinb123 <67251961+arinb123@users.noreply.github.com>
2021-02-27 11:07:06 -0800

    Homepage done

--- homepage.py
+++ homepage.py
@@ -1,6 +1,7 @@
 import pygame
 pygame.init()
 
+mode = 0
 screen = pygame.display.set_mode([1080, 620])
 run = True
 background = pygame.image.load('Background.jpg')
@@ -12,10 +13,21 @@ goodbutton2 = pygame.transform.scale(badbutton2, (282, 93))
 logo1 = pygame.image.load('Spacescape logo.png')
 logo2 = pygame.transform.scale(logo1, (784, 346))
 
+level = 0
+
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            if 689 > pos[0] > 409 and 451 > pos[1] > 363:
+                mode = 1
+                print(mode)
+            if 687 > pos[0] > 409 and 581 > pos[1] > 493:
+                mode = 2
+                print(mode)
+
     screen.fill((255, 255, 255))
     screen.blit(bg2, (-100, 0))
     screen.blit(goodbutton1, (408, 360))
