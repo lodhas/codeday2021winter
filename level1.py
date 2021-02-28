6a4a962bdfe82333d9625044734705c1ddc3eba5
Sarthak Lodha <sarthaklodha15@gmail.com>
2021-02-27 23:11:55 -0800

    HOMEPAGE WORKS LETS ACUTALLY GO WE HAVE REACHED THE FINAL STAGE OF OUR pLAN BUT WERE RUNNING OUT OF O2 IF THIS WORKS THEN WE WILL NOT COME OUT OF THIS ALIVE BUT I JUST WANT YOU TO KNOW THAT I AM STILL HIGH ON THAI FOOD

--- level1.py
+++ level1.py
@@ -77,7 +77,6 @@ asteroidX.append(random.randint(0, 824))
 asteroidY.append(random.randint(0, 150))
 asteroidY_change.append(random.randint(2, 3) / 10)
 
-
 # score
 
 font = pygame.font.Font('freesansbold.ttf', 32)
@@ -93,7 +92,7 @@ alive = True
 def getscore():
     global alive
     global deathtime
-    score_value = int(((time.time() if alive else deathtime) - starttime)*100//1)
+    score_value = int(((time.time() if alive else deathtime) - starttime) * 100 // 1)
     return score_value
 
 
@@ -135,8 +134,67 @@ def death():
 
 deathtime = 0
 direction = False
-running = True
+running = False
+run = True
 shottime = 0
+
+badbutton1 = pygame.image.load('CodeDay Feb 2021 Button.png')
+goodbutton1 = pygame.transform.scale(badbutton1, (282, 93))
+# badbutton2 = pygame.image.load('CodeDay Feb 2021 Button 2.png')
+# goodbutton2 = pygame.transform.scale(badbutton2, (282, 93))
+logo1 = pygame.image.load('Spacescape logo.png')
+logo2 = pygame.transform.scale(logo1, (784, 346))
+helpicon1 = pygame.image.load('Help Icon.png')
+helpicon2 = pygame.transform.scale(helpicon1, (544, 246))
+
+status = 0
+
+
+def help():
+    thing = True
+    helptext = pygame.image.load('Help Text.png')
+    ht2 = pygame.transform.scale(helptext, (1080, 620))
+    screen.blit(ht2, (0, 0))
+    print("Salutations")
+    global status
+    status = 1
+    return
+
+
+def unhelp():
+    screen.fill((255, 255, 255))
+    screen.blit(background, (-100, 0))
+    screen.blit(goodbutton1, (408, 360))
+    screen.blit(logo2, (138, -10))
+    screen.blit(helpicon2, (750, -70))
+    global status
+    status = 0
+    return
+
+unhelp()
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            if 689 > pos[0] > 409 and 451 > pos[1] > 363 and status == 0:
+                mode = 1
+                status = 2
+                run = False
+                running = True
+            if 1058 > pos[0] > 975 and 85 > pos[1] > 11 and status == 0:
+                mode = 3
+                help()
+                pygame.display.flip()
+                print(mode)
+            if 1068 > pos[0] > 744 and 612 > pos[1] > 525 and status == 1:
+                print("Go to homepage")
+                unhelp()
+            else:
+                print(pos)
+    pygame.display.flip()
 starttime = time.time()
 while running:
     if alive:
